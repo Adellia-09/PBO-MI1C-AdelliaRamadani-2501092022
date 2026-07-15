6af65a6b973d6a413fbb7b2e5d19cb68422bbc74
Adellia-09 <adeliarmdni09@gmail.com>
2026-07-16 00:20:55 +0700

    Add files via upload

---
+++ Adellia160426/StudentRecordExampel.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Adellia160426;
+import Adellia090426.StudentRecord;
+/**
+ *
+ * @author user
+ */
+public class StudentRecordExampel {
+    public static void main(String[] args){
+        StudentRecord student = new StudentRecord();
+        student.jenisKelamin = "L";
+        student.print("");
+    }
+}
